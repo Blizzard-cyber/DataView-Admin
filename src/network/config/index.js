209b0dbef3e175371b111fce3c90f2b606edc038
Blizzard <369476975@qq.com>
2023-05-27 15:01:31 +0800

    删除baseUrl

--- src/network/config/index.js
+++ src/network/config/index.js
@@ -1,5 +1,5 @@
 const serverConfig = {
-    baseURL: "http://43.248.188.73:11234", // 请求基础地址
+    baseURL: "", // 请求基础地址
     useTokenAuthorization: true, // 是否开启 token 认证
 };
 export default serverConfig;
